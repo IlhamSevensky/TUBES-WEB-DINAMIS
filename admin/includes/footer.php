7ee935459f7e33cb6198a28ccb6a360a5406bcb1
Ilham Budi Prasetyo <budip.ilham@gmail.com>
2020-01-07 19:50:23 +0700

    first upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>By Kelompok ...</strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>By Kelompok ...</strong>
+    </div>
+</footer>
